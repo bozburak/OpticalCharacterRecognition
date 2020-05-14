0d602704e8748578adce128729abb6b9a481676e
Burak BOZ <boz-burak-trash@hotmail.com>
2020-05-15 00:07:34 +0300

    add Tesseract package.

---
+++ OpticalCharacterRecognition/OpticalCharacterRecognition/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tesseract;
+
+namespace OpticalCharacterRecognition.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Assets/img"), Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    using (var engine = new TesseractEngine(Server.MapPath(@"~/tessdata"), "eng", EngineMode.Default))
+                    {
+                        using (var image = new System.Drawing.Bitmap(file.InputStream))
+                        {
+                            using (var pix = PixConverter.ToPix(image))
+                            {
+                                using (var page = engine.Process(pix))
+                                {
+                                    ViewBag.Rec = page.GetText();
+                                }
+                            }
+                        }
+                    }
+                    ViewBag.Message = "File uploaded successfully";
+                    ViewBag.ImagePath = "/Assets/img/" + file.FileName;
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                    ViewBag.Image = null;
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+                ViewBag.Image = null;
+            }
+            return View();
+        }
+    }
+}
